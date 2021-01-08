a529f1d4fe923acd04ca1ba91617aeb4e12813ec
Bora Berke Sahin <boraberke99@gmai.com>
2021-01-09 02:27:32 +0300

    finished endgame checking

--- graphics.py
+++ graphics.py
@@ -1,25 +1,27 @@
 import tkinter as tk
 from tictactoe import TicTacToe
+from tictactoe import Squares
 WIDTH =400
 HEIGHT = WIDTH
 OFFSET = WIDTH/25
 game = TicTacToe()
-def draw_xox_table(canvas,w,h,offset):
-    canvas.create_line(w/3,offset,w/3,h-offset,width=2)
-    canvas.create_line(2*w/3,offset,2*w/3,h-offset,width=2)
-    canvas.create_line(offset,h/3,w-offset,h/3,width=2)
-    canvas.create_line(offset,2*h/3,w-offset,2*h/3,width=2)
-    canvas.configure(bg='gray')
+def draw_xox_table(canvas,w,h,offset,line_width,line_color):
+    canvas.create_line(w/3,offset,w/3,h-offset,width=line_width,fill=line_color)
+    canvas.create_line(2*w/3,offset,2*w/3,h-offset,width=line_width,fill=line_color)
+    canvas.create_line(offset,h/3,w-offset,h/3,width=line_width,fill=line_color)
+    canvas.create_line(offset,2*h/3,w-offset,2*h/3,width=line_width,fill=line_color)
+    canvas.configure(bg='black')
 def draw_next(canvas,x_pos,y_pos,w,h):
-        if (game.one_round(x_pos,y_pos)):
+        if (game.one_turn(x_pos,y_pos) == Squares.Cross):
             draw_cross(canvas,x_pos,y_pos,w,h)
         else:
-            draw_circle(canvas,x_pos,y_pos,w,h)    
+            draw_circle(canvas,x_pos,y_pos,w,h)
+            
         game.print_state()
 
 
 def draw_circle(canvas,x_pos,y_pos,w,h):
-    # to put o in the center
+    # to put circle in the center of the squares
     x_offset = w/12
     y_offset = h/12
     rectangle_width = w/3
@@ -28,10 +30,10 @@ def draw_circle(canvas,x_pos,y_pos,w,h):
     x_end = x_pos * rectangle_width + 3 * x_offset   
     y_start = y_pos * rectangle_height + y_offset
     y_end = y_pos * rectangle_height + 3 * y_offset 
-    canvas.create_oval(x_start,y_start,x_end,y_end)
+    canvas.create_oval(x_start,y_start,x_end,y_end,width=2,outline='orange')
     
 def draw_cross(canvas,x_pos,y_pos,w,h):
-    # to put o in the center
+    # to put cross in the center of the squares
     x_offset = w/12
     y_offset = h/12
     rectangle_width = w/3
@@ -40,8 +42,8 @@ def draw_cross(canvas,x_pos,y_pos,w,h):
     x_end = x_pos * rectangle_width + 3 * x_offset   
     y_start = y_pos * rectangle_height + y_offset
     y_end = y_pos * rectangle_height + 3 * y_offset 
-    canvas.create_line(x_start,y_start,x_end,y_end,width=2)
-    canvas.create_line(x_end,y_start,x_start,y_end,width=2)
+    canvas.create_line(x_start,y_start,x_end,y_end,width=2,fill='orange')
+    canvas.create_line(x_end,y_start,x_start,y_end,width=2,fill='orange')
 
 def clicked_position(x,y,w,h):
     rectangle_width = w/3
@@ -51,13 +53,14 @@ def clicked_position(x,y,w,h):
 def callback(event):
     print ("clicked at", event.x, event.y)
     x_pos,y_pos = clicked_position(event.x,event.y,WIDTH,HEIGHT)
+    game.print_state()
     if(game.legal(x_pos,y_pos)):
         draw_next(canvas,x_pos,y_pos,WIDTH,HEIGHT)
 
 root = tk.Tk()
 canvas = tk.Canvas(root,width=WIDTH,height=HEIGHT)
 canvas.update()
-draw_xox_table(canvas,WIDTH,HEIGHT,OFFSET)
+draw_xox_table(canvas,WIDTH,HEIGHT,OFFSET,2,'white')
 canvas.bind("<Button-1>", callback)
 canvas.pack()
 
--- tictactoe.py
+++ tictactoe.py
@@ -1,39 +1,108 @@
-CROSS=1
-CIRCLE=2
-EMPTY=0
+
 #initialize the first player as cross
-next_player = CROSS
+from enum import Enum 
+class Squares(Enum):
+    Cross = 1
+    Circle = 2
+    Empty = 0
+
 class TicTacToe:
+
     def __init__(self):
-        self.state = [[0 for i in range(3)]for j in range(3)]
-        self.next_player = CROSS
+        self.state = [[Squares.Empty for i in range(3)]for j in range(3)]
+        self.next_player = Squares.Cross
         self.turn = 0
+        self.gameStatus = 'play'
     '''
-    return True if cross is the next player and add it to the array
-    check if the state is an end state
+    Plays one turn of the current player, returns the current player
+    Then calls 'check_end_game' method.
     '''
-    def one_round(self,x,y):
+    def one_turn(self,x,y):
+        current_player = self.next_player
         if (self.legal(x,y)):
-            if(self.next_player == CROSS):
-                self.state[y][x] = CROSS
-                self.next_player = CIRCLE
-                return True
-            else:
-                self.state[y][x] = CIRCLE
-                self.next_player = CROSS
-                return False
             self.turn+=1
             print(self.turn)
+            if(current_player == Squares.Cross):
+                self.state[y][x] = Squares.Cross
+                self.next_player = Squares.Circle             
+            else:
+                self.state[y][x] = Squares.Circle
+                self.next_player = Squares.Cross
+            self.check_end_game(current_player)  
+        return current_player
+    '''
+    Check win and draw conditions:
+        Cross Wins: three of the squares are marked with cross in a horizontal,vertical or diagonal row
+        Circle Wins: three of the squares are marked with cross in a horizontal,vertical or diagonal row
+        Draw: number of turns are 9 and no winners
+    '''        
+    def check_end_game(self,current_player):
+        #check horizontally
+        sum_of_line = 0
+        for row in self.state:
+            for col in row:
+                # only current_player can win, if there is any other square that is not current_player, then there is no winning condition
+                if(col == current_player):
+                    sum_of_line += 1
+                else:
+                    sum_of_line = 0
+                    break   
+                # if sum is 3, current_player wins
+                if(sum_of_line==3):
+                    self.gameStatus=current_player      
+
+
+                
+        #check vertically
+        for i in range(len(self.state)):
+            for j in range(len(self.state[i])):
+                # empty square means no winning condition
+                if(self.state[j][i] == current_player):
+                    sum_of_line += 1
+                else:
+                    sum_of_line = 0
+                    break
+                # if sum is 3, current_player wins
+                if(sum_of_line==3):
+                    self.gameStatus=current_player
+
+
+        #check for one diagonal
+        for i in range(len(self.state)):
+                # empty square means no winning condition
+                if(self.state[i][i] == current_player):
+                    sum_of_line += 1
+                else:
+                    sum_of_line = 0
+                    break
+                # if sum is 3, current_player wins
+                if(sum_of_line==3):
+                    self.gameStatus=current_player
+
+        #check for the other diagonal
+        for i in range(len(self.state)):
+                # empty square means no winning condition
+                if(self.state[i][len(self.state)-1-i] == current_player):
+                    sum_of_line += 1
+                else:
+                    sum_of_line = 0
+                    break
+                # if sum is 3, current_player wins
+                if(sum_of_line==3):
+                    self.gameStatus=current_player
+
     def legal(self,x,y):
-        if (self.state[y][x] == 0):
+        # tile is empty
+        if (self.state[y][x] == Squares.Empty):
             return True
         else:
             return False
     def print_state(self):
         for row in self.state:
             for col in row:
-                print(col,end = " ")
+                print(col.value,end = " ")
             print()
+        print(f"Game Status: {self.gameStatus}")
         
     
     
